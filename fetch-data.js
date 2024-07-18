f74c44461c00c8fac8201ae9a7de0e4e5fee680d
Fraol-X <pfresh641@gmail.com>
2024-07-18 12:24:26 +0300

    Fetching Data from an API and Displaying It

---
+++ fetch-data.js
@@ -0,0 +1,29 @@
+async function fetchUserData() {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+    const dataContainer = document.getElementById('api-data');
+
+    try {
+        const response = await fetch(apiUrl);
+        const users = await response.json();
+
+        dataContainer.innerHTML = '';
+
+        const userList = document.createElement('ul');
+
+        users.forEach(user => {
+            const userData = document.createElement('li');
+            userData.textContent = user.name;
+            userList.appendChild(userData);
+        });
+
+        dataContainer.appendChild(userList);
+
+    }
+
+    catch (error) {
+        dataContainer.innerHTML = "Data failed to load";
+    }
+
+    }
+    
+document.addEventListener('DOMContentLoaded', fetchUserData);
